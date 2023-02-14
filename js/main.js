886a5550e6392fb4239ad1fd02e67adce9feb820
FerbaDev <elferbarron1@gmail.com>
2023-02-14 01:28:17 -0300

    se agregan funciones

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const continuar1 = "Te invito a pasar mi porfolio en GitHub!";
+const cancelar1 = "Gracias por pasar!"
+function continuarsi() {
+    document.getElementById('id-info').innerHTML = continuar1;
+}
+function continuarno() {
+    document.getElementById('id-info').innerHTML = cancelar1;
+}
