9c1986710f4b90f29c78d4904f6273897d31e005
FerbaDev <elferbarron1@gmail.com>
2023-04-09 17:58:16 -0300

    se cambian funciones a botones

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ const sol = document.querySelector("#botonModoClaro");
 const luna = document.querySelector("#dark");
 const body = document.body;
 const navbar = document.querySelector("#navbar");
-const borde = document.getElementById("masInfo");
+const cartelMasInfo = document.getElementById("masInfo");
+const bordesEstudios = document.querySelectorAll(".bordesEstudios");
+const botonContinuar = document.getElementById("info-continuar");
+const contenedorEstudios = document.getElementById("contenedorEstudios");
+
+
 
 sol.addEventListener("click", modoClaro);
 luna.addEventListener("click", modoOscuro);
@@ -14,48 +19,103 @@ luna.addEventListener("click", modoOscuro);
 function modoClaro() {
     body.classList.add("claro");
     navbar.classList.add("claro");
-    document.getElementById("masInfo").style.borderColor = "#212529";
+    cartelMasInfo.style.borderColor = "#212529";
 }
 function modoOscuro() {
     body.classList.remove("claro");
     navbar.classList.remove("claro");
+    cartelMasInfo.style.borderColor = "azure";
 }
 
 
+botonContinuar.addEventListener("click", mostrarMasInfo);
 
+function mostrarMasInfo() {
+    contenedorEstudios.innerHTML = "";
+    const estudios = document.createElement("div");
+    estudios.classList.add("estudios");
+    estudios.innerHTML = `
+    <fieldset class="fieldset">
+        <legend class="legend">Mis conocimientos:</legend>
+        <ul class="lista-estudios">
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Flex-box</li>
+            <li>Grids</li>
+            <li>Bootstrap</li>
+            <li>Sass</li>
+            <li>Responsive design</li>
+            <li>Accesibilidad</li>
+            <li>SEO</li>
+            <li>Git/Github</li>
+        </ul>
+    </fieldset>`;
+    contenedorEstudios.append(estudios);
 
+    const estudiando = document.createElement("div");
+    estudiando.classList.add("estudiando");
+    estudiando.innerHTML = `
+    <fieldset class="fieldset-est">
+        <legend class="legend-est">Estudiando:</legend>
+        <ul class="lista-estudios">
+            <li>JavaScript ES6</li>
+        </ul>
+    </fieldset>`;
+    contenedorEstudios.append(estudiando);
 
+    const aEstudiar = document.createElement("div");
+    aEstudiar.classList.add("aestudiar");
+    aEstudiar.innerHTML = `
+                <fieldset class="fieldset-aest">
+                    <legend class="legend-aest">A estudiar:</legend>
+                        <ul class="lista-aestudios">
+                            <li>React JS</li>
+                            <li>Ajax & Fetch</li>
+                            <li>Frameworks & NodeJS</li>
+                            <li>Python</li>
+                            <li>NPM</li>
+                            <li>Websockets</li>
+                            <li>SQL</li>
+                            <li>MongoDB</li>
+                            <li>Mongoose</li>
+                            <li>Twilio & OWASP</li>
+                            <li>GraphQL</li>
+                        </ul>
+                </fieldset>`;
+    contenedorEstudios.append(aEstudiar);
 
+    continuar.removeEventListener("click", mostrarMasInfo);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("theme").style.backgroundColor = bgcolor;
-// document.getElementById("theme").style.color = color;
-// document.getElementById("masInfo").style.border = borde;
-// document.getElementById("navbar").style.backgroundColor = bgcolor;
-// document.getElementById("barra").style.backgroundColor = bgcolor;
-
-
-
-//empieza funcion de mas info
-
-function continuar() {
-    document.getElementById('id-info1').style.display='block';
-    document.getElementById('id-info2').style.display='none';
 }
+
+
+const botonCancelar = document.getElementById("info-cancelar");
+
+botonCancelar.addEventListener("click", cancelar);
 
 function cancelar() {
-    document.getElementById('id-info2').style.display='block';
-    document.getElementById('id-info1').style.display='none';
+    contenedorEstudios.innerHTML = "";
+    const gracias = document.createElement("div");
+    gracias.classList.add("aestudiar");
+    gracias.innerHTML = `
+                <fieldset class="fieldset-aest">
+                        <ul class="lista-aestudios">
+                            <span>Muchas gracias por pasar!</span>
+                        </ul>
+                </fieldset>
+    `
+    contenedorEstudios.append(gracias);
+
+    botonCancelar.removeEventListener("click", cancelar);
+    
 }
+
+
+
+
+
+
+
+
+
 
